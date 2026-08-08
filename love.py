77bf9c2b604a5b212a38d3f1003d112d8951bf7e
montrinquite <rodaimontrinquite@gmail.com>
2026-08-08 19:12:31 +0800

    love

---
+++ love.py
@@ -0,0 +1,23 @@
+from turtle import *
+import math
+
+speed(0)
+bgcolor("black")
+color("hotpink")
+pensize(2)
+
+hideturtle()
+
+for i in range(600):
+    t = i / 20
+
+    x = 16 * math.sin(t) ** 3
+    y = 13 * math.cos(t) - 5 * math.cos(2 * t) - 2 * math.cos(3 * t) - math.cos(4 * t)
+
+    penup()
+    goto(x * 15, y * 15)
+    pendown()
+    if i % 1 == 0:
+        write("I love you", align="center", font=("Arial", 8, "bold"))
+
+done()
